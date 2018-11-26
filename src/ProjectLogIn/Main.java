40b4977cf3df32bac9047afd4b344be3427b3a8a
PutthipongPOONBUA <putthipong.p@ku.th>
2018-11-26 22:48:09 +0700

    edit student info

--- src/ProjectLogIn/Main.java
+++ src/ProjectLogIn/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,9 +12,12 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
-        primaryStage.setTitle("MAIN");
+        primaryStage.setTitle("Registration Program");
+        Image icon = new Image(getClass().getResourceAsStream("/photos/KU_SubLogo.png"));
+        primaryStage.getIcons().add(icon);
         primaryStage.setScene(new Scene(root, 1280, 720));
         primaryStage.show();
+
     }
 
 
